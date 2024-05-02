20f4c99d703f502ae306fda995f81aae44e800ef
Jane Swingler <jswingler007@gmail.com>
2024-05-01 23:14:14 -0700

    Clone graph

---
+++ src/main/java/org/example/CloneGraph.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+// LEETCODE DESCRIPTION: 133. Clone a Graph
+//Given a reference of a node in a connected undirected graph.
+//Return a deep copy (clone) of the graph.
+//Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+public class CloneGraph {
+
+    //class Node was part of the leetcode given code for solution
+    private class Node {
+        public int val;
+
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+    private HashMap<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return node;
+        }
+
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        Node cloneNode = new Node(node.val, new ArrayList<>());
+        visited.put(node, cloneNode);
+
+        for (Node neighbor : node.neighbors) {
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+        return cloneNode;
+    }
+
+}
